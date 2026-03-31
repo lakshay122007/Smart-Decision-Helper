146e88bd69d0e83ca66d38406b3ec14bb3f79c2c
lakshay122007 <lakshay11goyal@gmail.com>
2026-03-31 19:16:15 +0530

    Data showing function added

---
+++ script.js
@@ -0,0 +1,26 @@
+let results = document.getElementById("results")
+
+function showData(items){
+    results.innerHTML = ""
+
+    items.forEach((item) => {
+        let div = document.createElement("div")
+        div.className = "card"
+
+        let title = document.createElement("h3")
+        title.innerText = item.title
+
+        let price = document.createElement("p")
+        price.innerText = `Price ₹: ${item.price}`
+
+        let rating = document.createElement("p")
+        rating.innerText = `Rating: ${item.rating}`
+
+        div.appendChild(title)
+        div.appendChild(price)
+        div.appendChild(rating)
+
+        results.appendChild(div)
+
+    })
+}
